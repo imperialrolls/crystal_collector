69cb9a543b9f01fca923fb7262a78ccc80f7b1ab
Christopher Rolls <imperialrolls@gmail.com>
2017-11-15 02:49:59 -0700

    first night work

---
+++ assets/javascript/game.js
@@ -0,0 +1,45 @@
+// var collection 
+
+var collectorNumber = "";
+var crystalNumber = "";
+var wins = 0;
+var losses = 0;
+var playerScore = 0;
+
+var crystalOne
+var crystalTwo
+var crystalThree
+var crystalFour
+
+
+// functions
+
+function startgame() {
+	wins = 0;
+	losses = 0;
+
+}
+
+var crystalNumber = function() {
+	crystalNumber = Math.floor(Math.random() ((y-x) +1) +x)];
+	console.log(crystalNumber);
+}
+
+var collectorNumber = function() {
+	collectorNumber = [Math.floor(Math.random() ((y-x) +1) +x)];
+	console.log(collectorNumber);
+}
+
+collectorNumber();
+
+startgame();
+
+
+
+// displays 
+
+function displayResults() {
+document.getElementById("crystalNumber").innerHTML = crystalNumber;
+document.getElementById("wins").innerHTML = wins;
+document.getElementById("playerScore").innerHTML = ;
+document.getElementById("losses").innerHTML = losses;
