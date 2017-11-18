0e44ea7d2188c278cef81ef2b5846539048b5268
Christopher Rolls <imperialrolls@gmail.com>
2017-11-18 00:26:37 -0700

    final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,45 +1,112 @@
-// var collection 
+// GLOBAL VARIABLES (Accessible by all functions)
+// 
+
+// this is the computer selected number the player is trying to match
 
 var collectorNumber = "";
-var crystalNumber = "";
-var wins = 0;
-var losses = 0;
-var playerScore = 0;
 
-var crystalOne
-var crystalTwo
-var crystalThree
-var crystalFour
+// this computer selected value for each crystal
+
+var crystalNumber = [];
+
+// 
+
+var numberOfCrystals = 4;
+
+// this is the players total score from each crystal click
+
+var totalScore = 0;
 
 
-// functions
+// functions// Game counters
 
-function startgame() {
-	wins = 0;
-	losses = 0;
+var winCounter = 0;
+var lossCounter = 0;
 
+// functions// init
+
+function init() {
+	for (i = 0; i < numberOfCrystals; i++) { 
+   	    crystalNumber[i] = crystal_number();
+    }
+    collectorNumber = collector_number();
+    document.getElementById("collectorNumber").innerHTML = collectorNumber;
+    totalScore = 0;
 }
 
-var crystalNumber = function() {
-	crystalNumber = Math.floor(Math.random() ((y-x) +1) +x)];
+// functions// startgame
+
+
+function startgame(){}
+
+
+// random number generator for crystal images ????
+
+function crystal_number() {
+	
+var	crystalNumber = Math.floor(Math.random()*12) +1;
+
 	console.log(crystalNumber);
+	return crystalNumber;
 }
 
-var collectorNumber = function() {
-	collectorNumber = [Math.floor(Math.random() ((y-x) +1) +x)];
-	console.log(collectorNumber);
-}
+function collector_number() {
+	
+var	value = Math.floor(Math.random()*102) +19;
 
-collectorNumber();
+	console.log(value);
+	return value;
+}
 
 startgame();
 
+// defining the crystal onclick handlers 
+
+document.getElementById("crystal1").onclick = function(){
+	totalScore += crystalNumber[0];
+		updateCollector();
+
+};
+
+document.getElementById("crystal2").onclick = function(){
+	totalScore += crystalNumber[1];
+		updateCollector();
+};
+
+document.getElementById("crystal3").onclick = function(){
+	totalScore += crystalNumber[2];
+		updateCollector();
+
+};
+
+document.getElementById("crystal4").onclick = function(){
+	totalScore += crystalNumber[3];
+		updateCollector();
+
+};
+
+function updateCollector() {
+	document.getElementById("totalScore").innerHTML = totalScore;
+	if (totalScore == collectorNumber) {
+		winCounter += 1;
+		init();
+		displayResults();
+	} else if (totalScore > collectorNumber) {
+		lossCounter += 1;
+		init();
+		displayResults();
+	} 
+
+}
 
 
 // displays 
 
 function displayResults() {
-document.getElementById("crystalNumber").innerHTML = crystalNumber;
-document.getElementById("wins").innerHTML = wins;
-document.getElementById("playerScore").innerHTML = ;
-document.getElementById("losses").innerHTML = losses;
+document.getElementById("winCounter").innerHTML = winCounter;
+document.getElementById("totalScore").innerHTML = 0;
+document.getElementById("lossCounter").innerHTML = lossCounter;
+}
+
+init();
+displayResults();
